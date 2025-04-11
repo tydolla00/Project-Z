f9e9fd3733224c07ef7e6b64c471d618d53cc7a5
tydolla00 <ktyty9@aol.com>
2025-04-11 18:29:33 -0400

    may have found the error.

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,28 +1,28 @@
 "use server";
 
-import mythicalIslands from "@/scripts/mythical-islands/mythical-islands.json";
-import geneticApex from "@/scripts/genetic-apex/genetic-apex.json";
-import spaceTimeSmackDown from "@/scripts/space-time-smackdown/space-time-smackdown.json";
-import triumphantLight from "@/scripts/triumphant-light/triumphant-light.json";
-import shiningRevelry from "@/scripts/shining-revelry/shining-revelry.json";
+// import mythicalIslands from "@/scripts/mythical-islands/mythical-islands.json";
+// import geneticApex from "@/scripts/genetic-apex/genetic-apex.json";
+// import spaceTimeSmackDown from "@/scripts/space-time-smackdown/space-time-smackdown.json";
+// import triumphantLight from "@/scripts/triumphant-light/triumphant-light.json";
+// import shiningRevelry from "@/scripts/shining-revelry/shining-revelry.json";
 
-export async function getCards(dex: string): Promise<Dex[]> {
-  switch (dex) {
-    case "mythical-islands":
-      return mythicalIslands;
-    case "genetic-apex":
-      return geneticApex;
-    case "space-time-smackdown":
-      return spaceTimeSmackDown;
-    case "triumphant-light":
-      return triumphantLight;
-    case "shining-revelry":
-      return shiningRevelry;
+// export async function getCards(dex: string): Promise<Dex[]> {
+//   switch (dex) {
+//     case "mythical-islands":
+//       return mythicalIslands;
+//     case "genetic-apex":
+//       return geneticApex;
+//     case "space-time-smackdown":
+//       return spaceTimeSmackDown;
+//     case "triumphant-light":
+//       return triumphantLight;
+//     case "shining-revelry":
+//       return shiningRevelry;
 
-    default:
-      return [];
-  }
-}
+//     default:
+//       return [];
+//   }
+// }
 
 export type Dex = {
   set: {
