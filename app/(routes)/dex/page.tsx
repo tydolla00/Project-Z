ea3fc3b278a3bf30c6d5b63c9fdb535daa7556a1
tydolla00 <ktyty9@aol.com>
2025-04-12 11:35:53 -0400

    hm

--- app/(routes)/dex/page.tsx
+++ app/(routes)/dex/page.tsx
@@ -5,15 +5,13 @@ import { CardDex } from "@/components/Dex";
 // import { ScrollArea } from "@/components/ui/scroll-area";
 // import Link from "next/link";
 // import { Suspense } from "react";
-// import prisma from "@/prisma/db";
+import prisma from "@/prisma/db";
 
 // Look at the meta for the page on serebii
 export default async function Page() {
-  return <CardDex cards={[]} />;
-  // const cards = await prisma.card.findMany({
-  //   include: { details: true, set: true },
-  // });
-  // const cards = [] as any[];
+  const cards = await prisma.card.findMany({
+    include: { details: true, set: true },
+  });
   // const geneticApex = cards.filter((c) => c.set.setName === "Genetic Apex");
   // const mythicalIslands = cards.filter(
   //   (c) => c.set.setName === "Mythical Island",
@@ -27,7 +25,8 @@ export default async function Page() {
   // const shiningRevelry = cards.filter(
   //   (c) => c.set.setName === "Shining Revelry",
   // );
-  // console.log("CARDS", cards.length);
+  console.log("CARDS", cards.length);
+  return <CardDex cards={[]} />;
 
   // // const mythicalIslands = await getCards("mythical-islands");
   // // const geneticApex = await getCards("genetic-apex");
